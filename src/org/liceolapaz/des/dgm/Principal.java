716138e675fe584678f0dd8f9fde8e2cefb381ec
DiegoGarcia <diego.garcia.mino@gmail.com>
2020-02-11 11:43:38 +0100

    Clases creadas y ventana preparada

---
+++ src/org/liceolapaz/des/dgm/Principal.java
@@ -0,0 +1,12 @@
+package org.liceolapaz.des.dgm;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Ventana ventana = new Ventana();
+		ventana.setVisible(true);
+
+	}
+
+}
---
+++ src/org/liceolapaz/des/dgm/Ventana.java
@@ -0,0 +1,22 @@
+package org.liceolapaz.des.dgm;
+
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame {
+	
+	public Ventana() {
+		super();
+		setTitle("Documento nuevo");
+		setSize(1024,768);
+		setLocationRelativeTo(null);
+		setResizable(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		crearMenu();
+	}
+
+	private void crearMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
